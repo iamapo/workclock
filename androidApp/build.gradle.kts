a841fa65f955208fe72296b5db169fb100f07a17
iamapo <redenius.andre@googlemail.com>
2026-07-27 12:57:51 +0200

    bump version to 1.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.iamapo.timetracker"
         minSdk = libs.versions.min.sdk.get().toInt()
         targetSdk = libs.versions.target.sdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         manifestPlaceholders["appLabel"] = "WorkClock"
     }
 
